7e7ec149f3a6bc72bb73c10c68a84a23d390b098
Вадим Козыревский <v.kozyrevskiy@timeweb.ru>
2025-11-05 11:02:11 +0300

    Добавил поддержку итерации по OfficerOrder

--- src/domain/entities/officer.go
+++ src/domain/entities/officer.go
@@ -1,5 +1,10 @@
 package entities
 
+import "errors"
+
+// Дефолтное значение максимального числа дежурантов в очереди
+const DefaultMaxOfficerinOrderCount int32 = 10
+
 // Дежурант
 type Officer struct {
 	firstName     string
@@ -19,13 +24,90 @@ func NewOfficer(firstName, lastName, telegramLogin, phoneNumber string) *Officer
 }
 
 // Возвращает имя дежурного
-func (d *Officer) FirstName() string { return d.firstName }
+func (o *Officer) FirstName() string { return o.firstName }
 
 // Возвращает фамилию дежурного
-func (d *Officer) LastName() string { return d.lastName }
+func (o *Officer) LastName() string { return o.lastName }
 
 // Возвращает telegram логин дежурного
-func (d *Officer) TelegramLogin() string { return d.telegramLogin }
+func (o *Officer) TelegramLogin() string { return o.telegramLogin }
 
 // Возвращает номер телефона дежурного
-func (d *Officer) PhoneNumber() string { return d.phoneNumber }
+func (o *Officer) PhoneNumber() string { return o.phoneNumber }
+
+// Сравнивает дежуранто. Считаем, что дежуранты равны, если у них совпадает telegramLogin
+func (o *Officer) Equals(officer *Officer) bool { return o.telegramLogin == officer.telegramLogin }
+
+// Элемент очереди дежурантов
+type OfficerOrderNode struct {
+	value Officer           // Данные о текущем дежуранте
+	next  *OfficerOrderNode // Следующий дежурант
+}
+
+// Создает новый NewOfficerOrderNode
+func NewOfficerOrderNode(officer *Officer, next *OfficerOrderNode) *OfficerOrderNode {
+	return &OfficerOrderNode{
+		value: *officer,
+		next:  next,
+	}
+}
+
+// Заменяет next у OfficerOrderNode
+func (oon *OfficerOrderNode) SetNext(node *OfficerOrderNode) { oon.next = node }
+
+// Очередь дежурантов
+type OfficerOrder struct {
+	head            *OfficerOrderNode // Ссылка на последнего дежуранта в очереди
+	tail            *OfficerOrderNode // Ссылка на первого дежуранта в очереди
+	officerCount    int32
+	maxOfficerCount int32
+}
+
+// Создает пустой OfficerOrder
+func NewOfficerOrder(maxOfficerCount *int32) *OfficerOrder {
+	var maxCountValue int32
+	if maxOfficerCount == nil {
+		maxCountValue = DefaultMaxOfficerinOrderCount
+	} else {
+		maxCountValue = *maxOfficerCount
+	}
+	return &OfficerOrder{
+		maxOfficerCount: maxCountValue,
+	}
+}
+
+// Добавляет новый элемент в OfficerOrder
+// Добавляет новый элемент в OfficerOrder
+func (oo *OfficerOrder) AddNewOfficer(officer *Officer) error {
+	if oo.officerCount == oo.maxOfficerCount {
+		return errors.New("Officer count already has maximum value")
+	}
+
+	if oo.head == nil {
+		// Если список пустой, новый элемент ссылается сам на себя
+		newNode := NewOfficerOrderNode(officer, nil)
+		newNode.SetNext(newNode)
+		oo.head = newNode
+		oo.tail = newNode
+	} else {
+		// Вставляем новый элемент после head
+		newNode := NewOfficerOrderNode(officer, oo.tail)
+		oo.head.SetNext(newNode)
+		oo.head = newNode
+	}
+
+	oo.officerCount++
+	return nil
+}
+
+// Возвращает текущее значение количиства дежурантов
+func (oo *OfficerOrder) OfficerCount() int32 { return oo.officerCount }
+
+// Возвращает текущее значение максимального количества дежурантов
+func (oo *OfficerOrder) MaxOfficerCount() int32 { return oo.maxOfficerCount }
+
+// Возвращает количество доступных для заполнения слотов
+func (oo *OfficerOrder) AvailableSlots() int32 { return oo.maxOfficerCount - oo.officerCount }
+
+// Проверяет пуста ли очередь
+func (oo *OfficerOrder) IsEmpty() bool { return oo.head == nil }
